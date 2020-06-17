9d46ca25ef244b28e1a54808644a9d4af6de1be2
yuyunzhi <lambert1129@foxmail.com>
2020-06-17 18:40:46 +0800

    feat:add es8 string

---
+++ static/lesson4-3.js
@@ -0,0 +1,15 @@
+// for (let i = 1; i < 32; i++) {
+//   if (i < 10) {
+//     console.log(`0${i}`)
+//   } else {
+//     console.log(i)
+//   }
+// }
+
+for (let i = 1; i < 30020; i += 1000) {
+  console.log(i.toString().padEnd(5, '*#'))
+}
+
+for (let i = 1; i < 32; i++) {
+  console.log(i.toString().padStart(2, '0'))
+}
