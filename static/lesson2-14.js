77b0d6e3cf4ad49fcd89d7d517303147d00a81e0
yuyunzhi <lambert1129@foxmail.com>
2020-06-17 15:54:13 +0800

    feat:add import export

---
+++ static/lesson2-14.js
@@ -0,0 +1,11 @@
+import * as Mod from './lesson2-14-mod.js'
+
+console.log(Mod)
+// let test = new Mod.Test()
+// console.log(test.id)
+// let animal = new Mod.Animal()
+// console.log(animal.name)
+// let people = new Mod.default()
+// console.log(people.id)
+
+
